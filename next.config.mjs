c73d55c101b67bc709bbd40748920297522af549
Muhammad Usama Tauqir <141023502+usama-tauqir@users.noreply.github.com>
2026-07-27 22:21:10 +0500

    Refactor GitHub Pages configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,27 @@
-const isGitHubPages = process.env.DEPLOY_TARGET === "github-pages";
+const isGitHubPages =
+  process.env.DEPLOY_TARGET === "github-pages";
 
 const repositoryName =
-  process.env.GITHUB_REPOSITORY?.split("/")[1] || "";
+  process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
 
-const isUserSite = repositoryName.endsWith(".github.io");
+const isUserOrOrganizationSite =
+  repositoryName.endsWith(".github.io");
 
 const basePath =
-  isGitHubPages && repositoryName && !isUserSite
+  isGitHubPages &&
+  repositoryName &&
+  !isUserOrOrganizationSite
     ? `/${repositoryName}`
     : "";
 
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   output: isGitHubPages ? "export" : "standalone",
+
   basePath,
+
   trailingSlash: isGitHubPages,
+
   images: {
     unoptimized: isGitHubPages,
   },
